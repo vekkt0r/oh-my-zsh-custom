b61602f49726387a01e654504214c8bafc47239b
Adam Engstrom <adam.engstrom@arris.com>
2020-02-17 14:01:01 +0100

    git commit from nvim workaround

--- editor.zsh
+++ editor.zsh
@@ -1,3 +1,6 @@
 export EDITOR='vim'
 export SVN_EDITOR=$EDITOR
 export FPP_EDITOR='emacsclient -n'
+# Problem when launching git commit editor from tig from nvim
+# https://github.com/neovim/neovim/issues/9960#issuecomment-488269954
+export NVIM_RPLUGIN_MANIFEST=/home/aengstrom/.local/share/nvim/rplugin.vim 

d85ed67e1addbf9d9f4b76ccf5198de5ee36934a
Adam Engstrom <adam.engstrom@arris.com>
2020-10-06 13:04:48 +0200

    Global aliases

--- alias_common.zsh
+++ alias_common.zsh
@@ -6,4 +6,21 @@ alias ltr='ls -ltr'
 alias cf='c++filt'
 alias reboot='echo nope'
 alias tb='tmux set-buffer'
-alias tigf='tig --first-parent'
+alias tgf='tig --first-parent'
+alias tgs='tig status'
+
+# Global alias
+alias -g F='find . -name'
+alias -g G='| grep'
+alias -g NF='*(oc[1])'
+alias -g CF='| c++filt'
+alias -g XG='| xargs grep'
+
+# Suffix alias
+alias -s {h,cpp,log,txt}=less
+alias -s {html}=w3m
+
+# Functions
+gapp() {
+  git diff -U0 | grepdiff --output-matching=hunk -E $1 | git apply --cached --unidiff-zero
+}
